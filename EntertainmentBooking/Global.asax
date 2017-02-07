cdbc74e7db2fc4bb0f8b8944e49fe463ee5b5064
Niels Odgaard <nod@novicell.dk>
2017-02-07 13:05:19 +0100

    Add project files.

---
+++ EntertainmentBooking/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntertainmentBooking.MvcApplication" Language="C#" %>
---
+++ EntertainmentBooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntertainmentBooking.Startup))]
+namespace EntertainmentBooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

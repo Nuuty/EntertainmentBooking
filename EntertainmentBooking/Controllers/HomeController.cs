ff75f1fbb018732a9dbbf7bcfb8e8562ca3f7758
Niels Odgaard <nod@novicell.dk>
2017-02-21 11:43:49 +0100

    Added more includes

--- EntertainmentBooking/Controllers/HomeController.cs
+++ EntertainmentBooking/Controllers/HomeController.cs
@@ -23,13 +23,13 @@ namespace EntertainmentBooking.Controllers
 
         public ActionResult Arrangements()
         {
-            var models = DbContext.arrangements.Include(b => b.bands).ToList();
+            var models = DbContext.arrangements.Include(b => b.bands).Include(c => c.customers).ToList();
             return View(models);
         }
 
         public ActionResult Genres()
         {
-            var models = DbContext.genres.Include(b => b.bands).ToList();
+            var models = DbContext.genres.Include(b => b.bands).Include(m => m.bands.Select(band => band.members)).ToList();
             return View(models);
         }
     }
